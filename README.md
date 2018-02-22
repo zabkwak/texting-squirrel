be8ee7b9bb7054b60316875a1f191d4f584aa16f
Lukáš Mačuda <lukas.macuda@gmail.com>
2018-02-22 14:35:46 +0100

    jsdoc in md & text as separated file

--- README.md
+++ README.md
@@ -1,2 +1,169 @@
 # Texting Squirrel
 Module to format texts with variables and functions. It supports multiple dictionaries.
+
+## Installation
+```bash
+$ npm install texting-squirrel
+```
+
+## Usage
+```javascript
+import Text from 'texting-squirrel';
+
+console.log(Text.format('Hi, {0}!', 'developer')); // will output Hi, developer!
+```
+
+## Functions
+
+<dl>
+<dt><a href="#addDictionary">addDictionary(key, [dictionary])</a></dt>
+<dd><p>Adds the new dictionary.</p>
+</dd>
+<dt><a href="#getDictionary">getDictionary(key)</a></dt>
+<dd><p>Gets the dictionary by the key.</p>
+</dd>
+<dt><a href="#setDictionary">setDictionary(key)</a></dt>
+<dd><p>Sets the actual dictionary.</p>
+</dd>
+<dt><a href="#addFunction">addFunction(key, fn)</a></dt>
+<dd><p>Adds the function.</p>
+</dd>
+<dt><a href="#get">get(key, ...args)</a></dt>
+<dd><p>Formats the text from the set dictionary. The text is searched as a value in the dictionary by defined key. If the key is not found in set dictionary it tries to find the text in default dictionary.</p>
+</dd>
+<dt><a href="#format">format(text, ...args)</a> ⇒ <code>string</code></dt>
+<dd><p>Formats the text. Data to format must be in braces.
+If there&#39;s a number in braces the value to replace is searched in the args by order in array.
+If it&#39;s a function in the braces the function is called. First parameter accepts number and it&#39;s searched in the args.
+It it&#39;s a string in the braces the value to replace is searched in the set dictionary.</p>
+</dd>
+<dt><a href="#hasValue">hasValue(key)</a></dt>
+<dd><p>Checks if the value is in the dictionary.</p>
+</dd>
+<dt><a href="#isDefault">isDefault()</a></dt>
+<dd><p>Checks if the dictionary is default.</p>
+</dd>
+<dt><a href="#getKey">getKey()</a></dt>
+<dd><p>Gets the key of the dictionary.</p>
+</dd>
+<dt><a href="#getValue">getValue(key)</a></dt>
+<dd><p>Gets the value of the key in dictionary.</p>
+</dd>
+</dl>
+
+<a name="addDictionary"></a>
+
+## addDictionary(key, [dictionary])
+Adds the new dictionary.
+
+**Kind**: global function
+
+| Param | Type | Description |
+| --- | --- | --- |
+| key | <code>string</code> \| <code>Object.&lt;string, string&gt;</code> | Key with which is the dictionary accesible to format texts. If it's an object, the key is default. |
+| [dictionary] | <code>Object.&lt;string, string&gt;</code> | Key-value object for mapping the texts. |
+
+<a name="getDictionary"></a>
+
+## getDictionary(key)
+Gets the dictionary by the key.
+
+**Kind**: global function
+
+| Param | Type |
+| --- | --- |
+| key | <code>string</code> |
+
+<a name="setDictionary"></a>
+
+## setDictionary(key)
+Sets the actual dictionary.
+
+**Kind**: global function
+
+| Param | Type |
+| --- | --- |
+| key | <code>string</code> |
+
+<a name="addFunction"></a>
+
+## addFunction(key, fn)
+Adds the function.
+
+**Kind**: global function
+
+| Param | Type |
+| --- | --- |
+| key | <code>string</code> |
+| fn | <code>function</code> |
+
+<a name="get"></a>
+
+## get(key, ...args)
+Formats the text from the set dictionary. The text is searched as a value in the dictionary by defined key. If the key is not found in set dictionary it tries to find the text in default dictionary.
+
+**Kind**: global function
+
+| Param | Type | Description |
+| --- | --- | --- |
+| key | <code>string</code> | Key of the text in the dictionary. |
+| ...args | <code>\*</code> | Arguments to pass in format method. |
+
+<a name="format"></a>
+
+## format(text, ...args) ⇒ <code>string</code>
+If there's a number in braces the value to replace is searched in the args by orIf it's a function in the braces the function is called. First parameter acceptsIt it's a string in the braces the value to replace is searched in the set dictionary.
+
+**Kind**: global function
+
+| Param | Type |
+| --- | --- |
+| text | <code>string</code> |
+| ...args | <code>\*</code> |
+
+**Example** *(Passing variables)*
+```js
+Text.format('{0}', 'one'); // returns one
+Text.format('{0} {1} {2}', 'one', 'two', 'three'); // returns one two three
+```
+**Example** *(Count function)*
+```js
+Text.format('{count(0, script, scripts, scripts)}', 5); // returns 5 scripts
+```
+**Example** *(Gender function)*
+```js
+Text.format('{gender(0, He, She, He/She)} codes', 'female'); //returns She codes
+```
+<a name="hasValue"></a>
+
+## hasValue(key)
+Checks if the value is in the dictionary.
+
+**Kind**: global function
+
+| Param | Type |
+| --- | --- |
+| key | <code>string</code> |
+
+<a name="isDefault"></a>
+
+## isDefault()
+Checks if the dictionary is default.
+
+**Kind**: global function
+<a name="getKey"></a>
+
+## getKey()
+Gets the key of the dictionary.
+
+**Kind**: global function
+<a name="getValue"></a>
+
+## getValue(key)
+Gets the value of the key in dictionary.
+
+**Kind**: global function
+
+| Param | Type |
+| --- | --- |
+| key | <code>string</code> |
--- src/index.js
+++ src/index.js
@@ -1,163 +1,18 @@
-import Dictionary from './dictionary';
+import Text from './text';
 
-class Text {
-
-    /**
-     * @type {string}
-     */
-    _dictionary = Dictionary.DEFAULT;
-    /**
-     * @type {Object.<string, Dictionary>}
-     */
-    _dictionaries = {};
-
-    /**
-     * @type {Object.<string, Function>}
-     */
-    _functions = {};
-
-    /**
-     * Adds the new dictionary.
-     * 
-     * @param {string|Object.<string, string>} key Key with which is the dictionary accesible to format texts. If it's an object, the key is default.
-     * @param {Object.<string, string>} [dictionary] Key-value object for mapping the texts.
-     */
-    addDictionary(key, dictionary) {
-        if (typeof key !== 'string') {
-            dictionary = key;
-            key = Dictionary.DEFAULT;
+export default new Text()
+    .addFunction('count', (count, a, b, c) => {
+        if (count === 1) {
+            return `${count} ${a}`;
         }
-        this._dictionaries[key] = new Dictionary(key, dictionary);
-        return this;
-    }
-
-    /**
-     * Gets the dictionary by the key.
-     * 
-     * @param {string} key 
-     */
-    getDictionary(key = this._dictionary) {
-        return this._dictionaries[key];
-    }
-
-    /**
-     * Sets the actual dictionary.
-     * 
-     * @param {string} key 
-     */
-    setDictionary(key) {
-        if (!this._dictionaries[key]) {
-            console.warn(`Dictionary with key '${key}' doesn't exist`);
-            return;
+        if (count > 4 || count === 0) {
+            return `${count} ${c}`;
         }
-        this._dictionary = key;
-        return this;
-    }
-
-    /**
-     * Adds the function.
-     * 
-     * @param {string} key 
-     * @param {Function} fn 
-     */
-    addFunction(key, fn) {
-        this._functions[key] = fn;
-        return this;
-    }
-
-    /**
-     * Formats the text from the set dictionary. The text is searched as a value in the dictionary by defined key. If the key is not found in set dictionary it tries to find the text in default dictionary.
-     * 
-     * @param {string} key Key of the text in the dictionary.
-     * @param {...*} args Arguments to pass in format method.
-     */
-    get(key, ...args) {
-        let dictionary = this.getDictionary() || this.getDictionary(Dictionary.DEFAULT);
-        if (!dictionary) {
-            console.warn('No dictionary set');
-            return '';
+        return `${count} ${b}`;
+    }).addFunction('gender', (gender, a, b, c) => {
+        switch (gender) {
+            case 'male': return a;
+            case 'female': return b;
+            default: return c;
         }
-        if (!dictionary.hasValue(key) && !dictionary.isDefault()) {
-            dictionary = this.getDictionary(Dictionary.DEFAULT);
-        }
-        if (!dictionary.hasValue(key)) {
-            console.warn(`Key '${key}' not found in dictionary '${dictionary.getKey()}'`);
-            return '';
-        }
-        return this.format(dictionary.getValue(key), ...args);
-    }
-
-    /**
-     * Formats the text. Data to format must be in braces. 
-     * If there's a number in braces the value to replace is searched in the args by order in array.
-     * If it's a function in the braces the function is called. First parameter accepts number and it's searched in the args.
-     * It it's a string in the braces the value to replace is searched in the set dictionary.
-     * 
-     * @example <caption>Passing variables</caption>
-     * Text.format('{0}', 'one'); // returns one
-     * Text.format('{0} {1} {2}', 'one', 'two', 'three'); // returns one two three
-     * 
-     * @example <caption>Count function</caption>
-     * Text.format('{count(0, script, scripts, scripts)}', 5); // returns 5 scripts
-     * 
-     * @example <caption>Gender function</caption>
-     * Text.format('{gender(0, He, She, He/She)} codes', 'female'); //returns She codes
-     * 
-     * @param {string} text 
-     * @param {...*} args 
-     * @returns {string}
-     */
-    format(text, ...args) {
-        if (!text) {
-            return '';
-        }
-        const match = text.match(/{(.+?)\}/g);
-        if (match !== null) {
-            match.forEach((m) => {
-                if (/{\d+}/.test(m)) {
-                    const key = m.slice(1, -1);
-                    if (args[key] !== undefined) {
-                        text = text.replace(m, args[key]);
-                    }
-                } else if (/\w+\(.*\)/.test(m)) {
-                    // TODO validate?
-                    const fn = m.match(/(\w+)/)[0];
-                    // TODO validate?
-                    const params = m.match(/(\(.*\))/)[0].slice(1, -1).split(',').map((item) => item.trim());
-                    if (params.length && !isNaN(params[0])) {
-                        params[0] = args[params[0]];
-                    }
-                    const caller = this._functions[fn];
-                    if (caller) {
-                        text = text.replace(m, caller.apply(this, params));
-                    } else {
-                        console.warn(`Invalid function '${fn}'`);
-                    }
-                } else if (/{\w+}/.test(m)) {
-                    text = text.replace(m, this.get(m.slice(1, -1), ...args));
-                }
-            });
-        }
-        return text.replace(/{(.+)\}/, '');
-    }
-}
-
-const t = new Text();
-
-t.addFunction('count', (count, a, b, c) => {
-    if (count === 1) {
-        return `${count} ${a}`;
-    }
-    if (count > 4 || count === 0) {
-        return `${count} ${c}`;
-    }
-    return `${count} ${b}`;
-}).addFunction('gender', (gender, a, b, c) => {
-    switch (gender) {
-        case 'male': return a;
-        case 'female': return b;
-        default: return c;
-    }
-});
-
-export default t;
+    });
