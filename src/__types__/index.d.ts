4c5921598171f243b25fe259379aa58c8aefec2d
Lukáš Mačuda <lukas.macuda@gmail.com>
2018-09-10 16:12:36 +0200

    updated type definition

--- src/__types__/index.d.ts
+++ src/__types__/index.d.ts
@@ -1,5 +1,19 @@
 declare module 'texting-squirrel' {
 
+    class Dictionary {
+        static readonly DEFAULT = 'default';
+
+        constructor(key: string, data: { [key: string]: string });
+
+        hasValue(key: string): boolean;
+
+        isDefault(): boolean;
+
+        getKey(): string;
+
+        getValue(string: key): string;
+    }
+
     class Text {
 
         addDictionary(dictionary: { [key: string]: string }): this;
@@ -12,6 +26,7 @@ declare module 'texting-squirrel' {
 
         addFunction(name: string, fn: (...args: any[]) => string): this;
 
+        get(key: string): string;
         get(key: string, ...args: any[]): string;
 
         format(text: string, ...args: any[]): string;
